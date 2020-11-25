ae922165ea3471c171c72ceb61ff7b61335eab34
David Ison <github@dtison.net>
2020-11-25 09:25:48 -0500

    Transitions to ActiveAccounts.js for account info.

---
+++ src/accounts/ActiveAccounts.example.js
@@ -0,0 +1,32 @@
+const activeAccounts = [
+  {
+    name: 'BTC',
+    id: 'bitcoin',
+    qty: 1
+  },
+  {
+    name: 'LTC',
+    id: 'litecoin',
+    qty: 1
+  },
+  {
+    name: 'BCH',
+    id: 'bitcoin-cash',
+    qty: 1
+  },
+  {
+    name: 'BSV',
+    id: 'bitcoin-cash-sv',
+    qty: 1
+  },
+  {
+    name: 'ETH',
+    id: 'ethereum',
+    qty: 1
+  },
+
+
+
+];
+
+export default activeAccounts;
--- src/components/Estimate.js
+++ src/components/Estimate.js
@@ -9,19 +9,18 @@ const Estimate = props => {
 
     <div className="flex flex-wrap overflow-hidden">
     {props.accounts.map((account, index) => {
-      const value = parseFloat(AES.decryptFromBase64(account.type, props.password));
 
       return (
       <div key={index} className="w-1/3 overflow-hidden">
-      {account.name} ${(value * account.price).toLocaleString('en-US', {maximumFractionDigits:2})}
+      {account.name} ${(account.qty * account.price).toLocaleString('en-US', {maximumFractionDigits:2})}
       </div>  );  
     })}
 
     <div className="w-1/3 overflow-hidden">
      Total ${
       props.accounts.reduce((acc,curr) => {
-        const value = parseFloat(AES.decryptFromBase64(curr.type, props.password));
-          return acc + (value * curr.price);
+
+          return acc + (curr.qty * curr.price);
       }, 0).toLocaleString('en-US', {maximumFractionDigits:2})
     }
   </div>
@@ -35,3 +34,7 @@ const Estimate = props => {
 
 export default Estimate;
  // ${account.price.toLocaleString('en-US', {maximumFractionDigits:2})} 
+
+ //const value = parseFloat(AES.decryptFromBase64(curr.type, props.password));
+
+// const value = parseFloat(AES.decryptFromBase64(account.type, props.password));
